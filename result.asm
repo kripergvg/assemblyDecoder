078e06d30e083d5cb8d7c95d8dc1a69fd5c6aa34
Vladimir Ivanov <wkololo_4ever@mail.ru>
2023-04-19 11:48:44 +0400

    arithmetic

--- result.asm
+++ result.asm
@@ -1,3 +1,24 @@
-mov [bx + di], cx
-mov [bp + si], cl
-mov [bp], ch
+jne 2
+jne -4
+jne -6
+jne -4
+je -2
+jl -4
+jle -6
+jb -8
+jbe -10
+jp -12
+jo -14
+js -16
+jne -18
+jnl -20
+jnle -22
+jnb -24
+jnbe -26
+jnp -28
+jno -30
+jns -32
+loop -34
+loopz -36
+loopnz -38
+jcxz -40
